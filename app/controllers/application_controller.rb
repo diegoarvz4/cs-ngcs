80c811a89db4f91a8dcd108e2879cf24ad180297
diegoarvz4 <darvz.gtz@gmail.com>
2019-07-29 22:58:09 -0500

    Initial Setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+
+    def index 
+    end 
+
+    def new 
+
+    end 
+
+end
---
+++ test/controllers/cleaner_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CleanerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
